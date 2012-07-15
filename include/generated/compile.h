71a9c92d55cb664a589af5d2bea03d60bc2f1a53
agat63 <xta0407@gmail.com>
2012-07-14 17:34:41 -0700

    added smartass2 governor to source.

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
 /* This file is auto generated, version 2 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#2 SMP PREEMPT Sat Jul 14 02:09:24 PDT 2012"
-#define LINUX_COMPILE_BY "AGAT_v0.1.1"
+#define UTS_VERSION "#2 SMP PREEMPT Sat Jul 14 11:18:49 PDT 2012"
+#define LINUX_COMPILE_BY "AGAT_v0.2.1"
 #define LINUX_COMPILE_HOST " "
 #define LINUX_COMPILER "gcc version 4.4.1 (Sourcery G++ Lite 2009q3-68) "
--- include/generated/utsrelease.h
+++ include/generated/utsrelease.h
@@ -1,1 +1,1 @@
-#define UTS_RELEASE "3.0.15AGAT-g6cd35a2-dirty"
+#define UTS_RELEASE "3.0.15_AGAT-gb8e152b-dirty"
